7f400c44d4908bf8987543855821074658a968f0
Jiajie Chen <c@jia.je>
2021-12-05 00:38:55 +0800

    Fix seq compare test

--- src/test/test_seq_compare.cpp
+++ src/test/test_seq_compare.cpp
@@ -4,22 +4,22 @@
 #include "tcp.h"
 
 int main(int argc, char *argv[]) {
-  assert(tcp_seq_lt(0x0000, 0x0001));
-  assert(tcp_seq_lt(0xFFFF, 0x0000));
-  assert(tcp_seq_lt(0x1000, 0x2000));
-  assert(tcp_seq_lt(0xF000, 0xFFFF));
+  assert(tcp_seq_lt(0x00000000, 0x00000001));
+  assert(tcp_seq_lt(0xFFFFFFFF, 0x00000000));
+  assert(tcp_seq_lt(0x00001000, 0x00002000));
+  assert(tcp_seq_lt(0xFFFFF000, 0xFFFFFFFF));
 
-  assert(tcp_seq_le(0x0000, 0x0001));
-  assert(tcp_seq_le(0x0000, 0x0000));
-  assert(tcp_seq_le(0xF000, 0xFFFF));
-  assert(tcp_seq_le(0xFFFF, 0x0000));
-  assert(tcp_seq_le(0xF000, 0x0000));
+  assert(tcp_seq_le(0x00000000, 0x00000001));
+  assert(tcp_seq_le(0x00000000, 0x00000000));
+  assert(tcp_seq_le(0xFFFFF000, 0xFFFFFFFF));
+  assert(tcp_seq_le(0xFFFFFFFF, 0x00000000));
+  assert(tcp_seq_le(0xFFFFF000, 0x00000000));
 
-  assert(tcp_seq_gt(0x0001, 0x0000));
-  assert(tcp_seq_gt(0x0000, 0xFFFF));
+  assert(tcp_seq_gt(0x00000001, 0x00000000));
+  assert(tcp_seq_gt(0x00000000, 0xFFFFFFFF));
 
-  assert(tcp_seq_ge(0x1234, 0x0000));
-  assert(tcp_seq_ge(0xFFFF, 0xF000));
+  assert(tcp_seq_ge(0x00001234, 0x0000));
+  assert(tcp_seq_ge(0xFFFFFFFF, 0xFFFFF000));
 
   return 0;
 }
