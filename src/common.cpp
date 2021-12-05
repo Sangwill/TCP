ad5c12259a129e01ba930693b4fd645568e2d436
Jiajie Chen <c@jia.je>
2021-12-05 15:24:08 +0800

    Fix typo

--- src/common.cpp
+++ src/common.cpp
@@ -327,7 +327,7 @@ int open_device(std::string tun_name) {
   }
 
   std::string command = "ifconfig '" + tun_name + "' up";
-  printf("Runnning: %s\n", command.c_str());
+  printf("Running: %s\n", command.c_str());
   system(command.c_str());
 
   // from macOS's perspective
@@ -355,14 +355,14 @@ int open_device(std::string tun_name) {
     tun_name = ifr.ifr_name;
 
     std::string command = "ip link set dev '" + tun_name + "' up";
-    printf("Runnning: %s\n", command.c_str());
+    printf("Running: %s\n", command.c_str());
     system(command.c_str());
 
     // from linux's perspective
     // local/remote is reversed
     command = std::string("ip addr add ") + remote_ip + " peer " + local_ip +
               " dev '" + tun_name + "'";
-    printf("Runnning: %s\n", command.c_str());
+    printf("Running: %s\n", command.c_str());
     system(command.c_str());
   }
 
